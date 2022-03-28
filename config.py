abb675d1a22d44ea1627b42236c2b873846a994a
kshitija236 <kshitija236@gmail.com>
2022-03-28 13:28:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ class SearchConfig(BaseConfig):
         parser.add_argument('--init_channels', type=int, default=16)
         parser.add_argument('--layers', type=int, default=8, help='# of layers')
         parser.add_argument('--seed', type=int, default=2, help='random seed')
-        parser.add_argument('--workers', type=int, default=4, help='# of workers')
+        parser.add_argument('--workers', type=int, default=1, help='# of workers')
         parser.add_argument('--alpha_lr', type=float, default=3e-4, help='lr for alpha')
         parser.add_argument('--alpha_weight_decay', type=float, default=1e-3,
                             help='weight decay for alpha')
@@ -94,7 +94,7 @@ class AugmentConfig(BaseConfig):
         parser.add_argument('--init_channels', type=int, default=36)
         parser.add_argument('--layers', type=int, default=20, help='# of layers')
         parser.add_argument('--seed', type=int, default=2, help='random seed')
-        parser.add_argument('--workers', type=int, default=4, help='# of workers')
+        parser.add_argument('--workers', type=int, default=1, help='# of workers')
         parser.add_argument('--aux_weight', type=float, default=0.4, help='auxiliary loss weight')
         parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
         parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path prob')
